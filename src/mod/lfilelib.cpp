e4018a95591487a73883c615bbee064b212b43ee
Mingau-Projects <miguelro7m@gmail.com>
2026-07-29 19:15:12 -0300

    Fixed a bug where a file would be created on both the desired directory and on the root folder. Added `file.getchildren` and `file.getdescendants` to the library. `file.getdescentants` bugs filenames, needs fixing.

--- src/mod/lfilelib.cpp
+++ src/mod/lfilelib.cpp
@@ -13,8 +13,7 @@ constexpr std::string MODE_TEXT = "text";
 
 static int file_exists(lua_State* L)
 {
-    const char* str = luaL_checkstring(L, 1);
-    std::filesystem::path filepath = std::filesystem::path(str);
+    std::filesystem::path filepath = luaL_checkstring(L, 1);
 
     bool exists = std::filesystem::exists(filepath);
     lua_pushboolean(L, exists);
@@ -25,10 +24,11 @@ static int file_exists(lua_State* L)
 static int file_create(lua_State* L)
 {
     std::filesystem::path path = luaL_checkstring(L, 1);
-    std::filesystem::path parent = path.parent_path();
-
+    
     if (path.has_filename())
     {
+        std::filesystem::path parent = path.parent_path();
+
         std::error_code ec;
         std::filesystem::create_directories(parent, ec);
         if (ec)
@@ -40,7 +40,7 @@ static int file_create(lua_State* L)
             return 0;
         }
 
-        std::ofstream file(path.filename(), std::ios::out);
+        std::ofstream file(path, std::ios::out);
 
         if (!file.is_open())
         {
@@ -73,6 +73,15 @@ static int file_create(lua_State* L)
 static int file_read(lua_State* L)
 {
     std::filesystem::path path = luaL_checkstring(L, 1);
+    if (!std::filesystem::exists(path))
+    {
+        std::string errorMessage = "File at '" + path.generic_string() + "' does not exist.\n";
+        luaL_error(L, errorMessage.c_str());
+        lua_pop(L, 1);
+
+        return 0;
+    }
+
     std::string mode = luaL_optstring(L, 2, MODE_TEXT.c_str());
 
     std::ios::openmode openMode = std::ios::in;
@@ -88,7 +97,7 @@ static int file_read(lua_State* L)
         modeValid = true;
     }
 
-    if (!modeValid) 
+    if (!modeValid)
     {
         std::string errorMessage = "Attempt to open file '" + path.generic_string() + "' with unknown mode: '" + mode + "'.\n";
         luaL_error(L, errorMessage.c_str());
@@ -121,8 +130,18 @@ static int file_read(lua_State* L)
 static int file_write(lua_State* L)
 {
     std::filesystem::path path = luaL_checkstring(L, 1);
+    if (!std::filesystem::exists(path))
+    {
+        std::string errorMessage = "File at '" + path.generic_string() + "' does not exist.\n";
+        luaL_error(L, errorMessage.c_str());
+        lua_pop(L, 1);
+
+        return 0;
+    }
+
     std::string data = luaL_checkstring(L, 2);
-    std::string mode = luaL_optstring(L, 3, MODE_TEXT.c_str());
+    int position = luaL_checkinteger(L, 3);
+    std::string mode = luaL_optstring(L, 4, MODE_TEXT.c_str());
 
     std::ios::openmode openMode = std::ios::out;
     bool modeValid = false;
@@ -146,7 +165,7 @@ static int file_write(lua_State* L)
         return 0;
     }
 
-    std::ofstream file(path, std::ios::app | openMode);
+    std::ofstream file(path, std::ios::ate | openMode);
     if (!file.is_open())
     {
         std::string errorMessage = "Failed to open file '" + path.generic_string() + std::string("'.\n");
@@ -156,10 +175,96 @@ static int file_write(lua_State* L)
         return 0;
     }
 
-    file << data;
+    file.seekp(position, std::ios::beg);
+
+    file.write(data.data(), data.size());
     file.close();
 
     return 0;
+}
+
+static int file_isfolder(lua_State* L)
+{
+    std::filesystem::path dir = luaL_checkstring(L, 1);
+    lua_pushboolean(L, std::filesystem::is_directory(dir));
+
+    return 1;
+}
+
+static int file_isfile(lua_State* L)
+{
+    std::filesystem::path file = luaL_checkstring(L, 1);
+    lua_pushboolean(L, std::filesystem::is_regular_file(file));
+
+    return 1;
+}
+
+static int file_getchildren(lua_State* L)
+{
+    std::filesystem::path path = luaL_checkstring(L, 1);
+    if (!std::filesystem::exists(path))
+    {
+        std::string errorMessage = "Folder at '" + path.generic_string() + "' does not exist.\n";
+        luaL_error(L, errorMessage.c_str());
+        lua_pop(L, 1);
+
+        return 0;
+    }
+    
+    if (!std::filesystem::is_directory(path))
+    {
+        std::string errorMessage = "Expected path to result in a directory, but got " + std::string(std::filesystem::is_regular_file(path) ? "file" : "unknown");
+        luaL_error(L, errorMessage.c_str());
+        lua_pop(L, 1);
+
+        return 0;
+    }
+
+    lua_newtable(L);
+    size_t key = 0;
+
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        const char* entryPath = entry.path().generic_string().c_str();
+        lua_pushstring(L, entryPath);
+        lua_rawseti(L, -2, key++);
+    }
+
+    return 1;
+}
+
+static int file_getdescendants(lua_State* L)
+{
+    std::filesystem::path path = luaL_checkstring(L, 1);
+    if (!std::filesystem::exists(path))
+    {
+        std::string errorMessage = "Folder at '" + path.generic_string() + "' does not exist.\n";
+        luaL_error(L, errorMessage.c_str());
+        lua_pop(L, 1);
+
+        return 0;
+    }
+    
+    if (!std::filesystem::is_directory(path))
+    {
+        std::string errorMessage = "Expected path to result in a directory, but got " + std::string(std::filesystem::is_regular_file(path) ? "file" : "unknown");
+        luaL_error(L, errorMessage.c_str());
+        lua_pop(L, 1);
+
+        return 0;
+    }
+
+    lua_newtable(L);
+    size_t key = 0;
+
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(path))
+    {
+        const char* entryPath = entry.path().generic_string().c_str();
+        lua_pushstring(L, entryPath);
+        lua_rawseti(L, -2, key++);
+    }
+
+    return 1;
 }
 
 static const luaL_Reg filelib[] = {
@@ -167,6 +272,10 @@ static const luaL_Reg filelib[] = {
     {"exists", file_exists},
     {"read", file_read},
     {"write", file_write},
+    {"isfile", file_isfile},
+    {"isfolder", file_isfolder},
+    {"getchildren", file_getchildren},
+    {"getdescendants", file_getdescendants},
     {NULL, NULL}
 };
 
